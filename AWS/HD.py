4219c0f6318df4b636610b49ce112670c6d8cfaa
jeremyshen1987 <88565253+jeremyshen1987@users.noreply.github.com>
2022-02-14 21:57:02 -0700

    Add files via upload

---
+++ AWS/HD.py
@@ -0,0 +1,199 @@
+import requests
+from requests_html import HTMLSession
+from bs4 import BeautifulSoup
+import pandas as pd
+import datetime
+import pytz
+import csv
+from botocore.exceptions import ClientError
+
+
+Current_Time = datetime.datetime.now(tz=pytz.timezone('Canada/Mountain'))
+Current_Date = Current_Time.date()
+File_Name = str(Current_Date)
+
+
+def scrapper(property_url):
+
+    try:
+    
+        df = pd.read_html(property_url)
+
+        if len(df[0][1]) <5:
+            print('\nSkipped: Missing Critical Informatino: Land Size / Years Built\n')
+            return
+
+
+        baths = df[0][1][0]
+        beds = df[0][1][1]
+        floor_space = df[0][1][2].split('(')[1].split('s')[0].replace(',','')
+        land_size = df[0][1][3].split('(')[1].split('s')[0].replace(',','')
+        years_built = df[0][1][4]
+
+
+        if df[2]["Value"][0] == None:
+
+            print('cannot find transaction field')
+            last_sold='Unknown'
+            last_sold_date=df[1]["Date"][0]
+            
+        else:
+
+            last_sold = df[2]["Value"][0].replace('$','').replace(',','')
+            last_sold_date = df[2]["Date"][0]
+
+
+
+        
+        get_html = s.get(property_url).text
+        soup = BeautifulSoup(get_html,'lxml')
+
+        assessment = soup.find_all('p', class_ = 'chakra-text css-23jksx')
+        city_appraisal = assessment[1].text.replace('$','').replace(',','')
+
+        community = soup.find('a',class_ = 'chakra-button css-15ptba5').text
+
+        addr = soup.find('h1', class_ = 'chakra-text css-1qktfcj').text
+    
+
+
+
+        with open(f'{File_Name}.csv', 'a', newline='', encoding='utf-8') as f:
+
+            w = csv.writer(f, delimiter = '\t')
+            w.writerow([addr, community, years_built, beds, baths, floor_space, land_size, last_sold, last_sold_date, city_appraisal, File_Name, property_url])
+
+    except Exception as e:
+        print(e)
+
+
+
+
+def control_flow(page_num):
+
+    try:
+
+        while True:
+        
+            url = f'https://www.honestdoor.com/recently-sold/ab/calgary?page={page_num}'
+
+            source = requests.get(url).text
+            soup = BeautifulSoup(source,'lxml')
+
+            dat = soup.find_all('p',class_ = 'chakra-text css-1uss9mh')
+
+            days_ago_list = []
+
+            for li in dat:
+                days_ago = li.text.split('d ')[1].split(' d')[0]
+                days_ago_list.append(days_ago)
+
+
+            property_list = []
+
+            link = soup.find_all('a', class_ = 'chakra-link css-1lfxdey')
+
+            for every_url in link:
+                if 'property' in every_url['href']:
+
+                    absolute_url = 'https://www.honestdoor.com' + every_url['href']
+                    property_list.append(absolute_url)
+
+
+            for days, property_url in zip(days_ago_list,property_list):
+                if int(days) < 16:
+                    
+                    print(property_url)
+                    scrapper(property_url)
+                  
+
+            if int(days_ago_list[-1]) > 15:
+                print('\n\n Task complete!')
+                break
+
+
+            page_num+=1
+            print(f'\n\n\nMoving on to page: {page_num}\n\n\n')
+
+    except Exception as e:
+        print(e)
+
+
+
+
+login_url = 'https://honestdoor.auth0.com/u/login?state=hKFo2SB5T0h2XzhqSDBaeHlnRmZJRDlMR05vRV93bXZZdmpqbqFur3VuaXZlcnNhbC1sb2dpbqN0aWTZIGlXUmJDazdmbEtCTTRmdHhvMHpLRTNkOTRPblBWVERHo2NpZNkgR3Rsa1FHN2g3d2NxNjNhemlsQXZndkUxNjFPRklpSXk'
+
+payload = {
+
+    'state': 'hKFo2SB5T0h2XzhqSDBaeHlnRmZJRDlMR05vRV93bXZZdmpqbqFur3VuaXZlcnNhbC1sb2dpbqN0aWTZIGlXUmJDazdmbEtCTTRmdHhvMHpLRTNkOTRPblBWVERHo2NpZNkgR3Rsa1FHN2g3d2NxNjNhemlsQXZndkUxNjFPRklpSXk',
+    'username':'moonmana@gmail.com',
+    'password':'Tcgxg@760119',
+    'action': 'default'
+
+}
+
+
+Pg_Num = 1
+
+with requests.session() as s:
+
+    s.post(login_url, data = payload)
+
+    with open(f'{File_Name}.csv', 'a', newline='', encoding='utf-8') as f:
+
+        w = csv.writer(f, delimiter = '\t')
+        w.writerow(['addr', 'community', 'years_built', 'beds', 'baths', 'floor_space', 'land_size', 'last_sold', 'last_sold_date', 'city_appraisal', 'record_date','link'])
+
+    control_flow(Pg_Num)
+
+
+
+
+
+
+
+
+
+    
+
+
+
+
+
+
+# r = s.get(list_url)
+# soup = BeautifulSoup(r.content, 'lxml')
+
+# transaction_table = soup.find('table', class_ = 'chakra-table css-5605sr')
+
+# for tb in transaction_table.find_all('tbody'):
+#     rows = tb.find_all('tr')
+#     for row in rows:
+#         trans_data = row.find('td',class_ = 'css-xumdn4').text
+#         print(trans_data)
+
+
+
+# for index,value in enumerate(df):
+#     print('**********************')
+#     print(index)
+#     print(value)
+
+
+
+    
+
+    
+
+
+
+# session = HTMLSession()
+# request = session.get('https://www.honestdoor.com/recently-sold/ab/calgary?page=1')
+
+
+# source = requests.get(f'{Product_URL}').text
+
+# soup = BeautifulSoup(source,'lxml')
+
+
+# Product_Info = soup.find('div', class_="mt-6 px-3 lg:px-0")
---
+++ AWS/csv_to_dynamo.py
@@ -0,0 +1,67 @@
+import datetime
+import pytz
+import csv
+import boto3
+from botocore.exceptions import ClientError
+
+
+Current_Time = datetime.datetime.now(tz=pytz.timezone('Canada/Mountain'))
+Current_Date = Current_Time.date()
+File_Name = str(Current_Date)
+
+dynamodb = boto3.resource('dynamodb')
+
+try:
+
+    with open(f'{File_Name}.csv', newline='') as csvfile:
+
+
+        table = dynamodb.Table('calgary_re')
+
+        with table.batch_writer() as batch:
+
+            rows = csv.DictReader(csvfile, delimiter = '\t')
+            for row in rows:
+                batch.put_item(
+                        Item={
+
+                            'record_date': row['record_date'],
+                            'years_built': row['years_built'],
+                            'addr': row['addr'],
+                            'community': row['community'],                          
+                            'beds': row['beds'],
+                            'baths': row['baths'],
+                            'floor_space': row['floor_space'],
+                            'land_size': row['land_size'],
+                            'last_sold': row['last_sold'],
+                            'last_sold_date': row['last_sold_date'],
+                            'city_appraisal': row['city_appraisal'],
+                            'link': row['link']
+                        }
+                )
+        print('csv uploaded!')
+
+
+except Exception as e:
+    print(e)
+
+
+def upload_file(file_name, bucket, object_name=None):
+
+
+    if object_name is None:
+        object_name = file_name
+
+
+    s3_client = boto3.client('s3')
+
+    try:
+        response = s3_client.upload_file(file_name, bucket, object_name)
+        print('File upload successful!')
+
+    except ClientError as e:
+        print(e)
+
+
+upload_file(f'{File_Name}.csv', 'csv-drop-box')
+
