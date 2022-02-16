a762ef9827506b93fe2c71334cf29aee58c032e7
jeremyshen1987 <88565253+jeremyshen1987@users.noreply.github.com>
2022-02-15 21:30:59 -0700

    Update Lambda_SES_Dynamo.py

--- AWS/Lambda_SES_Dynamo.py
+++ AWS/Lambda_SES_Dynamo.py
@@ -1,6 +1,7 @@
 import json
 import urllib.parse
 import datetime
+# pytz requires an layer setup in lambda
 import pytz
 import boto3
 from boto3.dynamodb.conditions import Key, Attr
@@ -9,6 +10,7 @@ from boto3.dynamodb.conditions import Key, Attr
 s3 = boto3.client('s3')
 ses = boto3.client('ses')
 
+# Must specify timezone since the server hosting lambda could be in east coast, which will roll to the next day
 Current_Time = datetime.datetime.now(tz=pytz.timezone('Canada/Mountain'))
 Current_Date = str(Current_Time.date())
 
@@ -16,6 +18,8 @@ Current_Date = str(Current_Time.date())
 
 
 def lambda_handler(event, context):
+    
+    # Gather bucket name and file name(key) from s3 events. 
 
     bucket = event['Records'][0]['s3']['bucket']['name']
     key = urllib.parse.unquote_plus(event['Records'][0]['s3']['object']['key'], encoding='utf-8')
@@ -25,12 +29,14 @@ def lambda_handler(event, context):
     
     try:
         
+        #record_date is the partition key,using query instead of scan to save the cost
         Dynamo_Response = table.query(
         KeyConditionExpression=Key('record_date').eq(f'{Current_Date}') 
         )
 
 
         items = Dynamo_Response['Items']
+        # use SES later on to notify user how many records are uploaded and a few samples for assurance.
         item_count = len(items)
         top_10 = items[:10]
             
